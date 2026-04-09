02869d36ad33c0a989952bf3602eb78b0ee857cf
Bartosz Sadowski <bartek_sadowski@o2.pl>
2026-04-09 22:05:59 +0200

    feat(nvim): Add language support

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,7 +16,7 @@ vim.opt.relativenumber = true
 -- Disable mouse
 vim.opt.mouse = ''
 
- -- Enable break indent
+-- Enable break indent
 vim.opt.breakindent = true
 
 -- Save undo history
@@ -56,7 +56,7 @@ vim.opt.scrolloff = 10
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function() vim.hl.on_yank() end,
-  desc = "Briefly highlight yanked text"
+  desc = 'Briefly highlight yanked text',
 })
 
 -- ##################################################
@@ -114,7 +114,7 @@ local gh = function(x) return 'https://github.com/' .. x end
 -- *****               COLORSCHEME              *****
 -- **************************************************
 
-vim.pack.add({gh('catppuccin/nvim')})
+vim.pack.add { gh 'catppuccin/nvim' }
 
 require('catppuccin').setup {}
 
@@ -124,3 +124,79 @@ vim.cmd.colorscheme 'catppuccin-frappe'
 -- Configure highlights
 vim.cmd.hi 'Comment gui=none'
 
+-- **************************************************
+-- *****                   LSP                  *****
+-- **************************************************
+
+-- Install Mason for LSP handling
+vim.pack.add { gh 'mason-org/mason.nvim' }
+
+require('mason').setup()
+
+-- Remove unused default mappings
+vim.keymap.del('n', 'grn')
+vim.keymap.del('n', 'gra')
+vim.keymap.del('n', 'grr')
+vim.keymap.del('n', 'gri')
+
+vim.api.nvim_create_user_command('LspInfo', 'checkhealth vim.lsp', {})
+
+vim.lsp.config('*', {
+  on_attach = function(client, bufnr)
+    print 'attached'
+    -- Navigation
+    -- Enable once telescope is installed
+    -- vim.keymap.set('n', 'gd', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]efinition' })
+    -- vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
+
+    -- Actions
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = '[R]e[n]ame' })
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ction' })
+
+    client.server_capabilities.semnticTokensProvider = vim.NIL
+  end,
+})
+
+-- Map servers to exes and enable
+local lsp_servers = {
+  bashls = 'bash-language-server',
+  cssls = 'vscode-css-language-server',
+  cucumber_language_server = 'cucumber-language-server',
+  eslint = 'vscode-eslint-language-server',
+  gradle_ls = 'gradle-language-server',
+  groovyls = 'groovy-language-server',
+  intelephense = 'intelephense',
+  lua_ls = 'lua-language-server',
+  ts_ls = 'typescript-language-server',
+}
+
+local function enableLSPs()
+  for server_name, lsp_executable in pairs(lsp_servers) do
+    if vim.fn.executable(lsp_executable) == 1 then vim.lsp.enable { server_name } end
+  end
+end
+enableLSPs()
+
+-- **************************************************
+-- *****               AUTOFORMAT               *****
+-- **************************************************
+
+vim.pack.add { gh 'stevearc/conform.nvim' }
+
+require('conform').setup {
+  notify_on_error = false,
+  format_on_save = function(bufnr)
+    -- Disable "format_on_save lsp_fallback" for languages that don't
+    -- have a well standardized coding style. You can add additional
+    -- languages here or re-enable it for the disabled ones.
+    local disable_filetypes = { c = true, cpp = true, cucumber = true }
+    return {
+      timeout_ms = 500,
+      lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+    }
+  end,
+  formatters_by_ft = {
+    lua = { 'stylua' },
+    javascript = { 'prettierd', 'prettier', stop_after_first = true },
+  },
+}
