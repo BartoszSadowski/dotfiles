62f5165904d4989726ad9e46ce1879fe6eccec00
Bartosz Sadowski <bartek_sadowski@o2.pl>
2026-04-10 19:26:17 +0200

    feat(nvim): add treesitter support

--- nvim/init.lua
+++ nvim/init.lua
@@ -49,6 +49,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Do not close folds by default
+vim.o.foldlevelstart = 99
+
 -- ##################################################
 -- #####          GENERAL AUTOCOMMANDS          #####
 -- ##################################################
@@ -129,8 +132,146 @@ vim.cmd.colorscheme 'catppuccin-frappe'
 vim.cmd.hi 'Comment gui=none'
 
 -- ##################################################
+-- #####                 TOOLS                  #####
+-- ##################################################
+
+-- **************************************************
+-- *****                  GIT                   *****
+-- **************************************************
+
+vim.pack.add { gh 'lewis6991/gitsigns.nvim' }
+
+require('gitsigns').setup {
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+  on_attach = function(bufnr)
+    local gitsigns = require 'gitsigns'
+
+    local function map(mode, keys, command, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, keys, command, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal { ']c', bang = true }
+      else
+        gitsigns.nav_hunk 'next'
+      end
+    end, { desc = 'Jump to next git [c]hange' })
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal { '[c', bang = true }
+      else
+        gitsigns.nav_hunk 'prev'
+      end
+    end, { desc = 'Jump to previous git [c]hange' })
+
+    -- Actions
+    map('v', '<leader>gs', function() gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = '[g]it hunk [s]tage' })
+    map('n', '<leader>gs', gitsigns.stage_hunk, { desc = '[g]it hunk [s]tage' })
+
+    map('v', '<leader>gr', function() gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = '[g]it hunk [r]eset' })
+    map('n', '<leader>gr', gitsigns.reset_hunk, { desc = '[g]it hunk [r]eset' })
+
+    map('n', '<leader>gS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+    map('n', '<leader>gu', gitsigns.undo_stage_hunk, { desc = 'git [u]ndo stage hunk' })
+    map('n', '<leader>gR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+    map('n', '<leader>gp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+    map('n', '<leader>gb', gitsigns.blame_line, { desc = 'git [b]lame line' })
+    map('n', '<leader>gd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+    map('n', '<leader>gD', function() gitsigns.diffthis '@' end, { desc = 'git [D]iff against last commit' })
+
+    -- Toggles
+    map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
+    map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
+  end,
+}
+
+-- ##################################################
+-- #####                UTILITY                 #####
+-- ##################################################
+
+-- **************************************************
+-- *****               AUTOPAIRS                *****
+-- **************************************************
+
+vim.pack.add { gh 'windwp/nvim-autopairs' }
+
+require('nvim-autopairs').setup()
+
+-- ##################################################
 -- #####               LANGUAGES                #####
 -- ##################################################
+--
+-- **************************************************
+-- *****               TREESITTER               *****
+-- **************************************************
+
+-- Manage TreeSitter definitions
+
+vim.pack.add { {
+  src = gh 'nvim-treesitter/nvim-treesitter',
+  version = 'main',
+} }
+
+-- Install defaults
+local defaultTreesitterParsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+require('nvim-treesitter').install(defaultTreesitterParsers)
+
+-- Setup languages
+
+---@param buf integer
+---@param language string
+local function treesitter_try_attach(buf, language)
+  -- check if parser exists and load it
+  if not vim.treesitter.language.add(language) then return end
+
+  -- enables syntax highlighting and other treesitter features
+  vim.treesitter.start(buf, language)
+
+  -- enables treesitter based folds
+  -- for more info on folds see `:help folds`
+  vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+  vim.wo.foldmethod = 'expr'
+
+  -- enables treesitter based indentation
+  vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+end
+
+local available_parsers = require('nvim-treesitter').get_available()
+local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    local buf, filetype = args.buf, args.match
+
+    local language = vim.treesitter.language.get_lang(filetype)
+    if not language then return end
+
+    if vim.tbl_contains(installed_parsers, language) then
+      -- enable the parser if it is installed
+      treesitter_try_attach(buf, language)
+    elseif vim.tbl_contains(available_parsers, language) then
+      -- if a parser is available in `nvim-treesitter` auto install it, and enable it after the installation is done
+      require('nvim-treesitter').install(language):await(function() treesitter_try_attach(buf, language) end)
+    else
+      -- try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
+      treesitter_try_attach(buf, language)
+    end
+  end,
+})
+
+-- Show treestiter context for long methods
+vim.pack.add { gh 'nvim-treesitter/nvim-treesitter-context' }
 
 -- **************************************************
 -- *****              AUTOCOMPLETE              *****
@@ -223,80 +364,3 @@ require('conform').setup {
     javascript = { 'prettierd', 'prettier', stop_after_first = true },
   },
 }
-
--- ##################################################
--- #####                 TOOLS                  #####
--- ##################################################
-
--- **************************************************
--- *****                  GIT                   *****
--- **************************************************
-
-vim.pack.add { gh 'lewis6991/gitsigns.nvim' }
-
-require('gitsigns').setup {
-  signs = {
-    add = { text = '+' },
-    change = { text = '~' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-  },
-  on_attach = function(bufnr)
-    local gitsigns = require 'gitsigns'
-
-    local function map(mode, keys, command, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, keys, command, opts)
-    end
-
-    -- Navigation
-    map('n', ']c', function()
-      if vim.wo.diff then
-        vim.cmd.normal { ']c', bang = true }
-      else
-        gitsigns.nav_hunk 'next'
-      end
-    end, { desc = 'Jump to next git [c]hange' })
-
-    map('n', '[c', function()
-      if vim.wo.diff then
-        vim.cmd.normal { '[c', bang = true }
-      else
-        gitsigns.nav_hunk 'prev'
-      end
-    end, { desc = 'Jump to previous git [c]hange' })
-
-    -- Actions
-    map('v', '<leader>gs', function() gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = '[g]it hunk [s]tage' })
-    map('n', '<leader>gs', gitsigns.stage_hunk, { desc = '[g]it hunk [s]tage' })
-
-    map('v', '<leader>gr', function() gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' } end, { desc = '[g]it hunk [r]eset' })
-    map('n', '<leader>gr', gitsigns.reset_hunk, { desc = '[g]it hunk [r]eset' })
-
-    map('n', '<leader>gS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
-    map('n', '<leader>gu', gitsigns.undo_stage_hunk, { desc = 'git [u]ndo stage hunk' })
-    map('n', '<leader>gR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
-    map('n', '<leader>gp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-    map('n', '<leader>gb', gitsigns.blame_line, { desc = 'git [b]lame line' })
-    map('n', '<leader>gd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
-    map('n', '<leader>gD', function() gitsigns.diffthis '@' end, { desc = 'git [D]iff against last commit' })
-
-    -- Toggles
-    map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
-    map('n', '<leader>tD', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
-  end,
-}
-
--- ##################################################
--- #####                UTILITY                 #####
--- ##################################################
-
--- **************************************************
--- *****               AUTOPAIRS                *****
--- **************************************************
-
-vim.pack.add { gh 'windwp/nvim-autopairs' }
-
-require('nvim-autopairs').setup()
